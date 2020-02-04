3dd75adfe76b48f982d6bf3ffe3a7eee390ceb97
anilkumar <anilkumarsahoo17@gmail.com>
2020-02-04 17:09:43 +0530

    initial commit

---
+++ src/main/java/com/example/demo/controller/TestDemoConroller.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestDemoConroller {
+
+	@GetMapping("/")
+	public String s1() {
+		
+		System.out.println("test demo");
+		
+		return "anil test ";
+	}
+}
